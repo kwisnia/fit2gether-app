ec189e579d042ee421b874994922eb4bd584af94
210234 Hanna Kraska <210234@edu.p.lodz.pl>
2022-01-06 14:43:06 +0100

    android intl fix

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "intl";
+import "intl/locale-data/jsonp/en";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider as PaperProvider } from "react-native-paper";
 import { combinedDefaultTheme, combinedDarkTheme } from "./styles/theme";
